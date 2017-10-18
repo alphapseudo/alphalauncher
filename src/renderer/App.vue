0d662143f641a52d5c47fecae92ed1e4c54bfedd
Andrew Fake <afake518@gmail.com>
2017-10-17 22:28:34 -0400

    [WIP] Implement general settings form

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,28 +1,25 @@
 <template lang="pug">
-  #app
-    section#configuration.section
-      .columns
-        .column.is-one-third.navigation
-          h1.title AlphaLauncher
-          aside.menu
-            .menu-label Configuration
-              ul.menu-list
-                li
-                  router-link(to="/general") General
-                li
-                  router-link(to="/difficulty") Difficulty
-                li
-                  router-link(to="/missions") Missions
-                li
-                  router-link(to="/mods") Mods
-                li
-                  router-link(to="/logging") Logging
-                li
-                  router-link(to="/scripting") Scripting
-        .column
-          router-view
-    section#review.section
-      review
+  section.section#app
+    .columns.is-mobile
+      .column.is-one-third.navigation
+        h1.title AlphaLauncher
+        aside.menu
+          .menu-label Configuration
+            ul.menu-list
+              li
+                router-link(to="/general") General
+              li
+                router-link(to="/difficulty") Difficulty
+              li
+                router-link(to="/missions") Missions
+              li
+                router-link(to="/mods") Mods
+              li
+                router-link(to="/logging") Logging
+              li
+                router-link(to="/scripting") Scripting
+      .column.is-two-thirds
+        router-view#view
 </template>
 
 <script>
@@ -36,20 +33,26 @@
 
 <style lang="scss">
   @import "~bulma/sass/utilities/initial-variables";
+  @import "~font-awesome/css/font-awesome.min.css";
 
   $text: $white;
   $text-light: $grey-lighter;
   $text-strong: $white;
   $background: $grey;
+  $body-background-color: $grey-darker;
   
   $title-color: $white;
   $subtitle-color: $white-ter;
   
   $section-padding: 1.5rem;
 
+  $button-static-background-color: $grey-darker;
+
   $menu-item-color: $white;
   $menu-item-active-color: $black;
   $menu-item-active-background-color: $white-bis;
+
+  $label-color: $white-ter;
 
   html, body {
     height: 100%;
@@ -59,24 +62,38 @@
   }
   body {
     -webkit-app-region: drag;
-    background: $grey-darker;
     -webkit-user-select: none;
   }
-  a {
+  a, input, label {
     -webkit-app-region: no-drag;
   }
-  .columns {
-    height: 100%;
-  }
+  .columns { height: 100% }
   .navigation {
     border-right: 1px solid $grey;
   }
-  #configuration {
-    height: 50vh;
+  #app { 
+    height: 100vh;
+    ::-webkit-input-placeholder {
+      color: $grey-light;
+    }
+    input {
+      background: $black-ter;
+      border: 1px solid $black-ter;
+      color: $white;
+      &:active, &:focus {
+        border: 1px solid $blue;
+      }
+    }
+  }
+  #view {
+    height: 100vh;
   }
   #review {
+    height: 50vh;
     border-top: 1px solid $grey;
   }
 
   @import "~bulma";
+  @import "~bulma-switch/switch.sass";
+  @import "~bulma-divider/divider.sass";  
 </style>
--- src/renderer/components/ServerConfig.vue
+++ src/renderer/components/ServerConfig.vue
@@ -1,5 +1,34 @@
 <template lang="pug">
-  h2.subtitle General
+  #general
+    h2.subtitle General
+    .field.is-horizontal
+      .field-body
+        .field.is-expanded
+          p.control
+            label.label.is-small Server
+            input.input.is-small(type="text" maxlength="255" placeholder="Hostname")
+        .field.is-narrow
+          p.control
+            label.label.is-small Port
+            input.input.is-small(type="number" value="2302" placeholder="Port")
+        .field
+          p.control
+            label.label.is-small Max Players
+            input.input.is-small(type="number" value="8" min="1")
+    .field.is-horizontal
+      .field-body
+        .field
+          p.control
+            label.label.is-small Server Password
+            input.input.is-small(type="password" placeholder="Client connection password")
+        .field
+          p.control
+            label.label.is-small Admin Password
+            input.input.is-small(type="password" placeholder="Server admin password")
+    .field
+      input#useVon.switch.is-success.is-small(type="checkbox" name="von" checked)
+      label.label.is-small(for="useVon") Allow VON
+    
 </template>
 
 <script>
