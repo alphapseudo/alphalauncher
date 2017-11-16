faf692d0109278894e7856df1b5dd9acd6a2885c
Andrew Fake <afake518@gmail.com>
2017-11-15 22:54:54 -0500

    Repair build templating and added additonal param

--- src/renderer/lib/profile.js
+++ src/renderer/lib/profile.js
@@ -1,5 +1,6 @@
 import { mkdirp } from 'mkdirp';
 import Storage from 'electron-json-storage';
+import Path from 'path';
 import { promisifyAll } from 'bluebird';
 import System from './system';
 
@@ -13,7 +14,7 @@ class Profile {
   }
 
   static async getProfilePath(base, profile) {
-    const path = `${base}/AlphaLauncher/${profile}`;
+    const path = Path.normalize(`${base}/AlphaLauncher/${profile}`);
     await mkdirp(path);
     return path;
   }
@@ -33,6 +34,45 @@ class Profile {
   static async saveProfileStore(name, store) {
     await Storage.setAsync(`profile_${name}`, store);
     return true;
+  }
+
+  static formatParams(params) {
+    return Object.keys(params).reduce((reduced, param) => {
+      const value = params[param];
+      if (value) {
+        const formatted = _.isBoolean(value) ? `-${param}` : `-${param}=${value}`;
+        reduced.push(formatted);
+      }
+      return reduced;
+    }, []).join(' ');
+  }
+
+  static formatConfig(profile) {
+    const profiles = Path.normalize(`AlphaLauncher/${profile}`);
+    const serverCfg = Path.join(profiles, `${profile}.cfg`);
+    return `"-config=${serverCfg}" "-profiles=${profiles}" -name=${profile}"`;
+  }
+
+  static formatMods(mods) {
+    const enabled = mods.filter(mod => mod.enabled);
+    if (enabled.length > 0) {
+      const formatted = enabled.reduce((reduced, mod) => {
+        const path = Path.join(mod.path, mod.name);
+        reduced.push(path);
+        return reduced;
+      }, []).join(';');
+      return `"-mod=${formatted}"`;
+    }
+
+    return '';
+  }
+
+  static getParams(store) {
+    const { params, profile, mods } = store;
+    const serverParams = Profile.formatParams(params);
+    const configParams = Profile.formatConfig(profile);
+    const modParams = Profile.formatMods(mods.available);
+    return `${serverParams} ${configParams} ${modParams}`;
   }
 }
 
