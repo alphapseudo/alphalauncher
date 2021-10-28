fd3c17c8ca02c82060cfa5269c23e43725253e64
Andrew Fake <afake518@gmail.com>
2021-10-28 03:50:58 -0400

    Merged entry point view

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+// import Tippy from 'v-tippy';
+// import Toasted from 'vue-toasted';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// TODO Re-enabled these plugins
+// Vue.use(Tippy, {
+//   size: 'small',
+//   distance: 5,
+//   animation: 'perspective'
+// });
+
+// Vue.use(Toasted, {
+//   position: 'bottom-right',
+//   duration: 4000
+// });
 
 Vue.config.productionTip = false
 
