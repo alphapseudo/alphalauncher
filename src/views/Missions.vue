037d42b16ce9aeebc599511c8fd2c53735c3fac5
Andrew Fake <afake518@gmail.com>
2021-10-28 03:45:59 -0400

    Ported over views

---
+++ src/views/Missions.vue
@@ -0,0 +1,227 @@
+<template lang="pug">
+  #missions
+    .columns
+      .column
+        p.title.is-5 Missions
+        p.subtitle.is-6 Create server mission rotation
+      .column.is-narrow
+        p.is-size-7 Selected #[b {{ selectedCount }}] of #[b {{ missionCount }}] missions
+    .columns.no-margin-bottom
+      .column
+        .field.has-addons
+          p.control
+            a.button.is-small.is-info(@click="selectAll" :disabled="!hasMissions")
+              span.icon
+                i.fa.fa-check-square-o
+              span.text Select
+          p.control
+            a.button.is-small.is-info(@click="deselectAll" :disabled="!hasMissions")
+              span.icon
+                i.fa.fa-square-o
+              span.text Deselect
+          p.control
+            a.button.is-small.is-info(@click="invert" :disabled="!hasMissions")
+              span.icon
+                i.fa.fa-adjust
+              span.text Invert
+          p.control
+            a.button.is-small.is-success(@click="refresh")
+              span.icon(v-tippy="{delay: 500}" title="Refresh mission list")
+                i.fa.fa-refresh(:class="{'fa-spin': isRefreshing}")
+      .column.is-narrow
+        .field
+          p.control.has-icons-left
+            input.input.is-small(type="text" placeholder="Search..." v-model.trim="search" :disabled="!hasMissions")
+            span.icon.is-small.is-left
+              i.fa.fa-search
+    table.table.is-fullwidth(v-if="hasMissions")
+      thead
+        tr
+          th
+          th Mission
+          th Map
+          th Difficulty
+      draggable.table-list(v-model="missions" :options="options" element="tbody")
+        tr(
+          v-for="(mission, index) in missions"
+          :key="mission.name + mission.level"
+          :class="{isHidden: missesSearch(mission.name)}"
+        )
+          td
+            span.drag-handle
+              i.fa.fa-bars
+          td
+            input.is-checkbox.is-white.is-small(
+              type="checkbox"
+              :checked="mission.enabled"
+            )
+            label(@click="toggleMission(index)")
+              | {{ mission.name | decode }}
+          td {{ mission.level }}
+          td
+            .select.is-small
+              select(:value="mission.difficulty" @change="changeDifficulty(index, $event)")
+                option(value="recruit") Recruit
+                option(value="regular") Regular
+                option(value="veteran") Veteran
+                option(value="custom") Custom
+    .centered-placeholder(v-else)
+      p.title.is-6 No Missions Found
+      p.subtitle.is-7 Add missions to your #[b MPMissions] directory
+    .columns
+      .column
+        p.hint
+          | Use #[i.fa.fa-bars] to change the order of the list.
+          br
+          | Click on a mission #[i.fa.fa-check] to include it in the rotation.
+      .column.is-narrow
+          p.difficulty.is-size-7 Set Global Difficulty
+          .field.has-addons
+            p.control
+              a.button.is-small.is-warning.is-outlined(
+                @click="globalDifficulty('recruit')"
+                :disabled="!hasMissions"
+              )
+                | Recruit
+            p.control
+              a.button.is-small.is-warning.is-outlined(
+                @click="globalDifficulty('regular')"
+                :disabled="!hasMissions"
+              )
+                | Regular
+            p.control
+              a.button.is-small.is-warning.is-outlined(
+                @click="globalDifficulty('veteran')"
+                :disabled="!hasMissions"
+              )
+                | Veteran
+            p.control
+              a.button.is-small.is-warning.is-outlined(
+                @click="globalDifficulty('custom')"
+                :disabled="!hasMissions"  
+              )
+                | Custom
+  </template>
+
+<script>
+  import Draggable from 'vuedraggable';
+  Draggable.beforeDestroy = null;
+  
+  export default {
+    name: 'missions',
+    mounted() {
+      const $table = $('table');
+      const $bodyCells = $table.find('tbody tr:first').children();
+
+      const colWidth = $bodyCells.map(function map() {
+        return $(this).width();
+      }).get();
+
+      $table.find('thead tr').children().each((i, v) => {
+        $(v).width(colWidth[i]);
+      });
+    },
+    data() {
+      return {
+        search: '',
+        isRefreshing: false,
+        options: {
+          handle: '.drag-handle',
+          ghostClass: 'ghost',
+          animation: 0
+        }
+      };
+    },
+    computed: {
+      missions: {
+        get() {
+          return this.$store.state.missions.available;
+        },
+        set(missions) {
+          this.$store.commit('REORDER_MISSIONS', missions);
+        }
+      },
+      missionCount() {
+        return this.missions.length;
+      },
+      selectedCount() {
+        return this.missions.reduce((count, mission) => {
+          if (mission.enabled) count += 1;
+          return count;
+        }, 0);
+      },
+      hasMissions() {
+        return this.missionCount > 0;
+      }
+    },
+    methods: {
+      toggleMission(i) {
+        this.$store.commit('TOGGLE_MISSION', i);
+      },
+      selectAll() {
+        this.$store.commit('TOGGLE_ALL_MISSIONS', true);
+      },
+      deselectAll() {
+        this.$store.commit('TOGGLE_ALL_MISSIONS', false);
+      },
+      invert() {
+        this.$store.commit('INVERT_MISSIONS');
+      },
+      changeDifficulty(index, e) {
+        this.$store.commit('CHANGE_DIFFICULTY', { index, value: e.target.value });
+      },
+      globalDifficulty(value) {
+        this.$store.commit('CHANGE_ALL_DIFFICULTY', value);
+      },
+      missesSearch(name) {
+        if (this.search === '') return false;
+        const re = new RegExp(this.search, 'ig');
+        return !name.match(re);
+      },
+      async refresh() {
+        try {
+          this.isRefreshing = true;
+          await this.$store.dispatch('REFRESH_MISSIONS');
+          this.$toasted.success('Missions Successfully Refreshed');
+        } catch (e) {
+          this.$toasted.error('Error reading \'MPMissions\'. Please check permissions');
+        }
+
+        setTimeout(() => {
+          this.isRefreshing = false;
+        }, 500);
+      }
+    },
+    filters: {
+      decode(name) {
+        return decodeURIComponent(name);
+      }
+    },
+    components: { Draggable }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~bulma/sass/utilities/initial-variables";
+  
+  $check-green: #2fff78;
+
+  #missions {
+    select { 
+      background: rgba(240, 248, 255, 0);
+      border: 0;
+      option { background: $black-ter }
+      &:focus { box-shadow: none }
+    }
+  }
+  td:nth-child(1) { width: 5% }
+  td:nth-child(2) { width: 85% }
+  td:nth-child(3) { width: 5% }
+  td:nth-child(4) { width: 5% }
+  .is-checkbox[type="checkbox"].is-white:checked + label::after {
+    border-color: $check-green !important;
+  }
+  .button .text { margin-bottom: 1px }
+  .difficulty { margin-bottom: 0.2rem }
+</style>
+
---
+++ src/views/Scripting.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+  mixin tooltip(text)
+    span.icon.has-text-warning(
+      title=text
+      v-tippy="true"
+    )
+      i.fa.fa-info-circle
+
+  #logging
+    p.title.is-5 Scripting
+    p.subtitle.is-6 Apply server-side scripting
+    .field.has-addons
+      .control
+        label.label.is-small Server Command Password
+        input.input.is-small(type="password" maxlength="255" placeholder="Password" v-model.lazy="commandPass")
+    hr
+    .columns
+      .column
+        .field
+          .control
+            label.label.is-small Double ID Detected
+            input.input.is-small(type="text" placeholder="User with same ID detected" v-model.lazy="doubleId")
+        .field
+          .control
+            label.label.is-small User Connected
+            input.input.is-small(type="text" placeholder="User has connected" v-model.lazy="userCon")
+        .field
+          .control
+            label.label.is-small User Disconnected
+            input.input.is-small(type="text" placeholder="User has disconnected" v-model.lazy="userDisc")
+        .field
+          .control
+            label.label.is-small Hacked Data
+            input.input.is-small(type="text" placeholder="Signed .pbo modification detected" v-model.lazy="hacked")
+      .column
+        .field
+          .control
+            label.label.is-small Different Data
+            input.input.is-small(type="text" placeholder="Signed .pbo version differs from server" v-model.lazy="different")
+        .field
+          .control
+            label.label.is-small Unsigned Data
+            input.input.is-small(type="text" placeholder="Unisigned data detected" v-model.lazy="unsigned")
+        .field
+          .control
+            label.label.is-small Regular Check
+            input.input.is-small(type="text" placeholder="Periodically called for each user" v-model.lazy="regular")
+        
+</template>
+
+<script>
+  export default {
+    name: 'logging',
+    computed: {
+      commandPass: {
+        get() { return this.$store.state.config.serverCommandPassword; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['serverCommandPassword', v]); }
+      },
+      doubleId: {
+        get() { return this.$store.state.config.doubleIdDetected; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['doubleIdDetected', v]); }
+      },
+      userCon: {
+        get() { return this.$store.state.config.onUserConnected; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['onUserConnected', v]); }
+      },
+      userDisc: {
+        get() { return this.$store.state.config.onUserDisconnected; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['onUserDisconnected', v]); }
+      },
+      hacked: {
+        get() { return this.$store.state.config.onHackedData; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['onHackedData', v]); }
+      },
+      different: {
+        get() { return this.$store.state.config.onDifferentData; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['onDifferentData', v]); }
+      },
+      unsigned: {
+        get() { return this.$store.state.config.onUnsignedData; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['onUnsignedData', v]); }
+      },
+      regular: {
+        get() { return this.$store.state.config.regularCheck; },
+        set(v) { this.$store.commit('UPDATE_CONFIG', ['regularCheck', v]); }
+      }
+    }
+  };
+</script>
