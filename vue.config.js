bd37e9a76d2ca70867ecc3409bba8bd99e2654aa
Andrew Fake <afake518@gmail.com>
2021-10-28 06:13:32 -0400

    Updated product name

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
+      builderOptions: {
+        productName: "Alpha Launcher",
+        appId: "com.github.alphapseudo.alphalauncher",
+      },
       nodeIntegration: true
     }
   }
